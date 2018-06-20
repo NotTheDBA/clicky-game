cdc62dffe5b9d8bfba6c269b962a272dd0fc0278
Randy Reitz <rsreitz@outlook.com>
2018-06-20 19:26:37 -0400

    Pull in some starting files and images.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ImageCard from "./components/ImageCard";
+import Wrapper from "./components/Wrapper";
+import friends from "./force.json";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+const App = () => (
+  <Wrapper>
+    <h1 className="title">Friends List</h1>
+   { friends.map(element => (
+      
+    <ImageCard
+      name={element.name}
+      image={"./assets/img/" + element.image}
+    />
+    ))
   }
-}
+  </Wrapper>
+);
 
 export default App;
