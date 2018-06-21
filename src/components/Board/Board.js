edbb02522e5749fe1b20a9679732357f148f9ba2
Randy Reitz <rsreitz@outlook.com>
2018-06-21 10:54:19 -0400

    Fleshed out level images.

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -18,15 +18,16 @@ class Board extends React.Component {
 
     handleIncrement = (img) => {
         this.state.picks.find(o => o === img) ? (
-            this.setState({ score: 0, picks: [] })
+            this.setState({ score: 0, level: 1, picks: [] })
         ) : (
                 this.setState({
-                    score: this.state.score + 1,
+                    score: (this.state.score + 1) === (this.state.level + 1) * (this.state.level + 1) ? 0 : this.state.score + 1,
                     best: this.state.score + 1 > this.state.best ? this.state.score + 1 : this.state.best,
-                    picks: this.state.picks.concat(img)
+                    picks: (this.state.score + 1) === (this.state.level + 1) * (this.state.level + 1) ? [] : this.state.picks.concat(img),
+                    level: (this.state.score + 1) === (this.state.level + 1) * (this.state.level + 1) ? this.state.level + 1 : this.state.level
                 })
             )
-        console.log(this.state.picks)
+        // console.log(this.state.picks)
     };
 
     render() {
